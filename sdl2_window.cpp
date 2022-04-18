588b312a62d5ebb215f3e8fd642c8a98da6e00ec
Shvetsov196 <71310971+Shvetsov196@users.noreply.github.com>
2022-04-18 16:31:07 +0500

    Add files via upload

---
+++ sdl2_window.cpp
@@ -0,0 +1,17 @@
+
+
+#include "Window.h"
+#include "ZayacWindow.h"
+int main(int, char**)
+{
+	SDL_Init(SDL_INIT_EVERYTHING);
+	ZayacWindow window1;
+	window1.main_loop();
+
+
+
+
+
+
+	return 0;
+}
